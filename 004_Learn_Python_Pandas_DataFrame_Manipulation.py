eeb6a4050d46d496c0bcffcb087722ac6504d529
Naveen Silvester <silvester.naveen@gmail.com>
2025-05-21 14:30:48 +0530

    Added DF Manipulations

--- 004_Learn_Python_Pandas_DataFrame_Manipulation.py
+++ 004_Learn_Python_Pandas_DataFrame_Manipulation.py
@@ -102,5 +102,52 @@ print("\nAverage Salary by Department:")
 print(grouped)
 print("\nCount by Department:")
 grouped = df.groupby('Department').count().reset_index()
-print(grouped)      
+print(grouped)    
+print("\nCount by Department, Salary and Age:")  
+grouped = df.groupby('Department')[["Salary","Age"]].mean().reset_index()
+print(grouped)   
+df.groupby('Department')
+
+# Count of non-null Names by Department
+grouped_c = df.groupby('Department')['Name'].count().reset_index()
+print("\nCount of Names by Department:")
+print(grouped_c)
+
+# **Merge the count column properly**
+grouped_mean = grouped.merge(grouped_c, on="Department", how="left")
+grouped_mean.rename(columns={"Name": "counts"}, inplace=True)
+
+print("\nFinal DataFrame with Counts:")
+print(grouped_mean)
+
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+
+
+print ("""
+#######################################################################################################
+       Example-6: Merging and Joining
+       Use merge() or concat() to combine DataFrames.
+#######################################################################################################       
+""")
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', None],
+    'Age': [25, np.nan, 30, 22, 28],
+    'Salary': [50000, 60000, np.nan, 45000, 52000],
+    'Department': ['HR', 'IT', 'IT', 'Marketing', 'HR']
+}
+df = pd.DataFrame(data)
+
+data2 = {
+    'Department': ['HR', 'IT', 'Marketing'],
+    'Location': ['New York', 'San Francisco', 'Chicago']
+}
+df2 = pd.DataFrame(data2)
+print(f"\nDataFrame-1 is :\n{df}")
+print(f"\nDataFrame-2 is :\n{df2}")
+
+# Merge DataFrames on Department
+merged_df = pd.merge(df, df2, on='Department', how='left')
+print("\nMerged DataFrame (Merged based on Department):")
+print(merged_df)
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
